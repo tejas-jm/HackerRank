1e386c445e06ce1283610a1e6a2e8f9e93a20d3a
Tejas JM <93418229+Shisui-12@users.noreply.github.com>
2022-08-19 12:36:32 +0530

    Create Pangrams.py

---
+++ ProblemSolving/Pangrams.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
+    n = len(s)
+    s = s.lower()
+    print(s)
+    for i in range(26):
+            if (s.count(lst[i]) >= 1):
+                pass
+            else:
+                print(lst[i])
+                return "not pangram"
+    return "pangram"
+               
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
