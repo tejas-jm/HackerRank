c5594d01634aadeda19eb0e0124c51fe27828c01
Tejas JM <93418229+Shisui-12@users.noreply.github.com>
2022-08-20 20:36:09 +0530

    Create Beautiful_Days.py

---
+++ Beautiful_Days.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulDays' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER i
+#  2. INTEGER j
+#  3. INTEGER k
+#
+
+def beautifulDays(i, j, k):
+    # Write your code here
+    beautiful_num = 0
+    for n in range(i,j+1):
+        str1 = str(n)
+        str1 = str1[::-1]
+        rev = int(str1)
+        var = abs(n - rev)/k
+        if (int(var) == var):
+            beautiful_num += 1
+    return beautiful_num
+        
+            
+   
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    i = int(first_multiple_input[0])
+
+    j = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
