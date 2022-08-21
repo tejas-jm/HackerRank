da17de0219ea49627d7e9abd7f405b1c7b63750e
Tejas JM <93418229+Shisui-12@users.noreply.github.com>
2022-08-21 14:57:43 +0530

    Create Vector_Sort.cpp

---
+++ CPP/Vector_Sort.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */       vector <int> v;
+      int n;
+      cin >> n;
+      int x;
+      for (int i = 0; i < n; i++)
+      {
+          cin >> x;
+          v.push_back(x);
+      }
+      sort (v.begin(),v.end());
+      for(auto element : v)
+      {
+          cout << element << " ";
+      }
+    return 0;
+}
